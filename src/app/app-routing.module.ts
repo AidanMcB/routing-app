a342cdac0840121e807e4e68eff300ef7fd52fb3
aidan <amcbri22@gmail.com>
2021-01-17 00:57:10 -0500

    added routes, working on route error with wildcard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const routes: Routes = [
+   { path: 'first-component', component: FirstComponent }, 
+   { path: 'second-component', component: SecondComponent },
+   { path: '', redirectTo: '/first-component', pathMatch: 'full'}, //Redirects to first component
+   { path: '**', component: PageNotFoundComponent } //Wildcard route 404 page
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
